c2c8ac4cb5e5a80ed3e380453e3fd8ef358fa040
RamanaGithub <adari.vraman@gmail.com>
2021-07-06 22:06:04 +0530

    first commit in demo2 project

---
+++ src/main/java/com/mygit/demo2/git_demo2/App.java
@@ -0,0 +1,20 @@
+package com.mygit.demo2.git_demo2;
+
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
+//import com.example.demo.DemoApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	//public static Logger logger=LoggerFactory.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
